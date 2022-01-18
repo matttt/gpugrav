16e6809ab21e343849d99260560cf39733d81a89
Matthew Martori <matthew.martori@gmail.com>
2022-01-18 14:42:05 -0800

    kinda workig

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let universe;
 function setup() {
   createCanvas(800, 800);
+  // createCanvas(innerWidth, innerHeight)
 
   universe = new Universe();
 
@@ -10,7 +11,7 @@ function setup() {
 }
 
 function draw() {
-  console.log(frameRate())
+  // console.log(frameRate())
   translate(width/2, height/2)
   background(0)
   universe.computeGravityFrame()
